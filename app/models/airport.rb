82baf7adb93ab77c4cc63f4bda8d0f9aaa95ad99
Benjamin Pearce <admin@bcpearce.com>
2014-11-29 20:19:35 -0500

    added validations to Airport model

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,2 +1,8 @@
 class Airport < ActiveRecord::Base
+
+  validates :name, presence:true
+  validates :iata_faa, presence:true
+  validates :longitude, presence:true
+  validates :latitude, presence:true
+
 end
--- spec/models/airport_spec.rb
+++ spec/models/airport_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Airport, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validations" do
+
+    context "presence" do
+      it { should validate_presence_of :name }
+      it { should validate_presence_of :iata_faa }
+      it { should validate_presence_of :latitude }
+      it { should validate_presence_of :longitude }
+    end
+
+  end
+  
 end
