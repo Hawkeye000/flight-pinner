01aec1343e4d6e31d9382084d39414da40f90e55
Benjamin Pearce <admin@bcpearce.com>
2014-12-02 20:32:30 -0500

    added custom marker for airports map

--- app/controllers/airports_controller.rb
+++ app/controllers/airports_controller.rb
@@ -5,6 +5,11 @@ class AirportsController < ApplicationController
     @hash = Gmaps4rails.build_markers(@airports) do |airport, marker|
       marker.lat airport.latitude
       marker.lng airport.longitude
+      marker.picture({
+        url:"/assets/airplane21.png",
+        width:"24",
+        height:"24"
+      })
     end
   end
 
