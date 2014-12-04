6cd8fdedd1d2c42baac137da375fb9cbd605c06f
Benjamin Pearce <admin@bcpearce.com>
2014-12-03 22:04:51 -0500

    added controller and views for Routes

---
+++ spec/controllers/routes_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe RoutesController, :type => :controller do
+
+  before do
+    origin = create(:airport, iata_faa:"BOS")
+    destination = create(:airport, iata_faa:"JFK")
+    airline = create(:airline)
+  end
+
+  describe 'GET #index' do
+
+    it 'populates an array of all routes' do
+      route = create(:route)
+      get :index
+      expect(assigns(:routes)).to eq([route])
+    end
+
+    it 'renders the :index view' do
+      get :index
+      expect(response).to render_template :index
+    end
+
+  end
+
+  describe 'GET #show' do
+
+    it 'assigns the requested route to @route' do
+      route = create(:route)
+      get :show, id: route
+      expect(assigns(:route)).to eq(route)
+    end
+
+    it 'renders the :show template' do
+      get :show, id:create(:route)
+      expect(response).to render_template :show
+    end
+
+  end
+
+end
