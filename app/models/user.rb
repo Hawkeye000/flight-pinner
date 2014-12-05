5252349953f8f417ba7bae99f708cc806a605672
Benjamin Pearce <admin@bcpearce.com>
2014-12-04 22:54:52 -0500

    added associations between users and airports and airlines

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ActiveRecord::Base
 
   has_many :route_users
   has_many :routes, through: :route_users
+  has_many :destination_airports, through: :routes
+  has_many :origin_airports, through: :routes
+  has_many :airlines, through: :routes
 end
