452ed9c8f28b0fc1f96c70c8bb757d61950e8942
Benjamin Pearce <hawk.git@bcpearce.com>
2014-12-17 22:55:31 -0500

    added handling for route searches that turn up blank, included error message

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -19,19 +19,24 @@ class RoutesController < ApplicationController
       airports = [origin, destination]
     end
     @routes = @routes.page params[:page]
-    @airports = Gmaps4rails.build_markers([*airports]) do |airport, marker|
-      marker.lat airport.latitude
-      marker.lng airport.longitude
-      marker.picture({
-        url:"/assets/airplane21.png",
-        width:"32",
-        height:"32",
-        anchor:["12", "12"]
-        })
-        marker.infowindow render_to_string(partial:'shared/mapinfo',
-        locals:{airport:airport}).gsub(/\n/, '').gsub(/"/,'\"')
-      end
-    @polylines = @routes.map { |x| x.map_line } unless @routes.nil?
+
+    if @routes.empty?
+      flash[:alert] = "No Routes Found!"
+    else
+      @airports = Gmaps4rails.build_markers([*airports]) do |airport, marker|
+        marker.lat airport.latitude
+        marker.lng airport.longitude
+        marker.picture({
+          url:"/assets/airplane21.png",
+          width:"32",
+          height:"32",
+          anchor:["12", "12"]
+          })
+          marker.infowindow render_to_string(partial:'shared/mapinfo',
+          locals:{airport:airport}).gsub(/\n/, '').gsub(/"/,'\"')
+        end
+      @polylines = @routes.map { |x| x.map_line } unless @routes.nil?
+    end
 
   end
 
--- spec/features/home_features_spec.rb
+++ spec/features/home_features_spec.rb
@@ -17,15 +17,16 @@ describe "Home Page" do
   describe "searching for a route" do
 
     before do
+      @airline = create(:airline, name:"TestAirline")
+      @origin = create(:airport, iata_faa:"BOS")
+      @destination = create(:airport, iata_faa:"JFK")
+      @route = create(:route, origin_airport:@origin, destination_airport:@destination, airline:@airline)
+
       login_as create(:user), scope: :user
       visit root_path
     end
 
     it "should display the routes with matching params" do
-      airline = create(:airline, name:"TestAirline")
-      origin = create(:airport, iata_faa:"BOS")
-      destination = create(:airport, iata_faa:"JFK")
-      route = create(:route, origin_airport:origin, destination_airport:destination, airline:airline)
       fill_in "search_origin_field", with:"BOS"
       fill_in "search_destination_field", with:"JFK"
       fill_in "search_airline_field", with:"TestAirline"
@@ -34,6 +35,26 @@ describe "Home Page" do
       expect(page).to have_content("JFK")
       expect(page).to have_content("TestAirline")
     end
+
+    context "failed search" do
+      it "should display a flash message" do
+        fill_in "search_origin_field", with:"LGA"
+        fill_in "search_destination_field", with:"JFK"
+        fill_in "search_airline_field", with:"TestAirline"
+        click_link_or_button "Search Routes"
+      end
+
+      it "should not display any routes" do
+        expect(page).to_not have_content("BOS")
+        expect(page).to_not have_content("JFK")
+        expect(page).to_not have_content("TestAirline")
+      end
+    end
+
+    context "blank field" do
+      it "should not filter based on that field"
+    end
+
   end
 
   after do |example|
